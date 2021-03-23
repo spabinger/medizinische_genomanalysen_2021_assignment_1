4e17d5c01c3cfa0d706c59eb2935c7916e2572e0
spabinger <stephan.pabinger@gmail.com>
2021-03-23 20:46:10 +0100

    First setup of assignment1

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,5 @@
 import pysam
+import pybedtools
 
 __author__ = 'XXX'
 
@@ -7,60 +8,42 @@ __author__ = 'XXX'
 ## TODO
 ##
 
-
 class Assignment1:
-    
+
     def __init__(self):
-        ## The gene of interest
-        self.gene = "RRP1"
+        self.bamfile_path = "chr21_small.bam"
 
-    def get_coordinates_of_gene(self):
-        ## Use UCSC file
-        print("todo")
-        
+    def get_gene_information(self):
+        return {"symbol": "RRP1B",
+                "refseq": "NM_015056",
+                "chr": "chr21",
+                "tx_start": 43659559,
+                "tx_end": 43696079,
+                "strand": "+",
+                "num_exons": 16,
+                "exons_start": "43659559,43669883,43672307,43673869,43674635,43675033,43676271,43676732,43683278,43684552,43685769,43686803,43687515,43690287,43691438,43693189",
+                "exons_end": "43659794,43669966,43672365,43673955,43674697,43675163,43676336,43676914,43683373,43684650,43685789,43686935,43688240,43690440,43691502,43696079"
+                }
+
     def get_gene_symbol(self):
-        print("todo")
-        return "test"
-                        
-    def get_sam_header(self):
-        print("todo")
-        
-    def get_properly_paired_reads_of_gene(self):
-        print("todo")
-        
-    def get_gene_reads_with_indels(self):
-        print("todo")
-        
-    def calculate_total_average_coverage(self):
-        print("todo")
-        
-    def calculate_gene_average_coverage(self):
-        print("todo")
-        
-    def get_number_mapped_reads(self):
-        print("todo")
+        return None
 
+    ## Return: chr:start-end
     def get_region_of_gene(self):
-        print("todo")
-        
+        return None
+
     def get_number_of_exons(self):
-        print("ads")
-    
-    
-    def print_summary(self):
-        print("Print all results here")
-    
-    
-def main():
-    print("Assignment 1")
-    assignment1 = Assignment1()
-    assignment1.print_summary()
-    
-    
-    print("Done with assignment 1")
-    
-        
-if __name__ == '__main__':
-    main()
-    
-    
+        return None
+
+    def get_number_mapped_reads(self):
+        return None
+
+    def get_aligner_from_sam_header(self):
+        return None
+
+    def get_number_of_properly_paired_reads_of_gene(self):
+        return None
+
+    ## Use bedtools - genome_coverage
+    def calculate_average_genome_coverage_of_file(self):
+        return None
--- assignment1_test.py
+++ assignment1_test.py
@@ -1,7 +1,35 @@
-import assignment1;
+import assignment1
 
-def test_hello():
-    a1 = assignment1.Assignment1()
-    assert a1.get_gene_symbol() == "test"
+def test_gene_symbol():
+    a1 = assignment1_solution.Assignment1()
+    assert a1.get_gene_symbol() == "RRP1B"
+
+def test_get_region_of_gene():
+    a1 = assignment1_solution.Assignment1()
+    assert a1.get_region_of_gene() == "chr21:43659559-43696079"
+
+def test_get_number_of_exons():
+    a1 = assignment1_solution.Assignment1()
+    assert a1.get_number_of_exons() == 16
+
+def test_get_number_mapped_reads():
+    a1 = assignment1_solution.Assignment1()
+    assert a1.get_number_mapped_reads() == 11821
+
+def test_get_aligner_from_sam_header():
+    a1 = assignment1_solution.Assignment1()
+    assert a1.get_aligner_from_sam_header() == "novoalign"
+
+def test_get_number_of_properly_paired_reads_of_gene():
+    a1 = assignment1_solution.Assignment1()
+    assert a1.get_number_of_properly_paired_reads_of_gene() == 11624
+
+def test_calculate_average_genome_coverage_of_file():
+    a1 = assignment1_solution.Assignment1()
+    x = 47.15
+    assert x - 1 <= a1.calculate_average_genome_coverage_of_file() <= x + 1
+
+
+
 
 
